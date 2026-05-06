af940bcf5b627f91e2e07991906d0d638e703c2c
adamzuo <42708281+adamzuo@users.noreply.github.com>
2026-05-06 13:49:30 +0800

    Change default URL from HTTPS to HTTP

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://121.33.75.98:21114".to_owned()
 }
 
 #[inline]
